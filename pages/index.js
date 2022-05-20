7ce91c16c17744865cb0812c3474189bdadfc2ad
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-05-20 10:06:57 +0200

    Add first commit, add first important files and configuration

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
+import { Title } from '../components/header';
 
 export default function Home() {
   return (
     <>
-      <h1>Hello</h1>
+      <Title>Hello</Title>
     </>
   );
 }
