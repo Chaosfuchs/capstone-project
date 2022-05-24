814e5f312d66ddff34f7ce5ec7de3d15cc285912
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-05-24 12:03:31 +0200

    Insert link to characters page

--- pages/characters.js
+++ pages/characters.js
@@ -1,11 +1,15 @@
 import { Title } from '../components/Header';
 import ShowCharacter from '../components/ShowCharacter';
+import Link from 'next/link';
 
 export default function Characters() {
   return (
     <>
       <Title>My Characters</Title>
       <ShowCharacter />
+      <Link passHref href="/createCharacter">
+        <button as="a">Back to Create Character</button>
+      </Link>
     </>
   );
 }
--- pages/createCharacter.js
+++ pages/createCharacter.js
@@ -1,6 +1,7 @@
 import { Title } from '../components/Header';
 import AddForm from '../components/AddForm';
 import ShowCharacter from '../components/ShowCharacter';
+import Link from 'next/link';
 
 export default function AddCharacter() {
   return (
@@ -8,6 +9,9 @@ export default function AddCharacter() {
       <Title>Create Character</Title>
       <AddForm />
       <ShowCharacter />
+      <Link passHref href="/characters">
+        <button as="a">Show Characters</button>
+      </Link>
     </>
   );
 }
