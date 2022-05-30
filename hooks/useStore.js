ecb6219cb12bd1d21a9a62bbdffc161a195318a3
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-05-30 14:41:33 +0200

    Implement local storage via persist from middleware in zustand

--- hooks/useStore.js
+++ hooks/useStore.js
@@ -1,36 +1,39 @@
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const useStore = create(set => {
-  return {
-    characters: [],
+const useStore = create(
+  persist(set => {
+    return {
+      characters: [],
 
-    toasts: [
-      {
-        id: 1,
-        shown: false,
-        title: 'Saved',
-        description:
-          'Your Character was saved. Check it out under "Characters"',
+      toasts: [
+        {
+          id: 1,
+          shown: false,
+          title: 'Saved',
+          description:
+            'Your Character was saved. Check it out under "Characters"',
+        },
+      ],
+
+      addCharacter: newCharacter => {
+        set(state => {
+          return {
+            characters: [newCharacter, ...state.characters],
+          };
+        });
       },
-    ],
-
-    addCharacter: newCharacter => {
-      set(state => {
-        return {
-          characters: [newCharacter, ...state.characters],
-        };
-      });
-    },
-    toggleToast: (toastId, visible) => {
-      set(state => {
-        return {
-          toasts: state.toasts.map(toast =>
-            toast.id === toastId ? { ...toast, shown: visible } : toast
-          ),
-        };
-      });
-    },
-  };
-});
+      toggleToast: (toastId, visible) => {
+        set(state => {
+          return {
+            toasts: state.toasts.map(toast =>
+              toast.id === toastId ? { ...toast, shown: visible } : toast
+            ),
+          };
+        });
+      },
+    };
+  })
+);
 
 export default useStore;
