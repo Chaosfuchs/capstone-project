43d763c2a0e20846c02d9d3a7bc882f6e0621821
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-05-30 16:21:34 +0200

    Add local storage and prevent hydration error

--- hooks/useStore.js
+++ hooks/useStore.js
@@ -2,38 +2,41 @@ import create from 'zustand';
 import { persist } from 'zustand/middleware';
 
 const useStore = create(
-  persist(set => {
-    return {
-      characters: [],
+  persist(
+    set => {
+      return {
+        characters: [],
 
-      toasts: [
-        {
-          id: 1,
-          shown: false,
-          title: 'Saved',
-          description:
-            'Your Character was saved. Check it out under "Characters"',
+        toasts: [
+          {
+            id: 1,
+            shown: false,
+            title: 'Saved',
+            description:
+              'Your Character was saved. Check it out under "Characters"',
+          },
+        ],
+
+        addCharacter: newCharacter => {
+          set(state => {
+            return {
+              characters: [newCharacter, ...state.characters],
+            };
+          });
         },
-      ],
-
-      addCharacter: newCharacter => {
-        set(state => {
-          return {
-            characters: [newCharacter, ...state.characters],
-          };
-        });
-      },
-      toggleToast: (toastId, visible) => {
-        set(state => {
-          return {
-            toasts: state.toasts.map(toast =>
-              toast.id === toastId ? { ...toast, shown: visible } : toast
-            ),
-          };
-        });
-      },
-    };
-  })
+        toggleToast: (toastId, visible) => {
+          set(state => {
+            return {
+              toasts: state.toasts.map(toast =>
+                toast.id === toastId ? { ...toast, shown: visible } : toast
+              ),
+            };
+          });
+        },
+      };
+    },
+    { name: 'characters' }
+  )
 );
 
 export default useStore;
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,21 @@
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, theme } from '../components/GlobalStyles';
 import Toast from '../components/Toasts';
+import useHasHydrated from '../hooks/useHasHydrated';
 
 function MyApp({ Component, pageProps }) {
+  const isHydrated = useHasHydrated();
   return (
     <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-        <Toast />
-      </ThemeProvider>
+      {isHydrated && (
+        <>
+          <GlobalStyle />
+          <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+            <Toast />
+          </ThemeProvider>
+        </>
+      )}
     </>
   );
 }
