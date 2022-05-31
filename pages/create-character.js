a2fe4984abf629dd8d62d5454c2d1f88cc0e5d74
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-05-31 10:05:00 +0200

    Make that content is in the center

--- pages/create-character.js
+++ pages/create-character.js
@@ -1,13 +1,20 @@
 import { Title } from '../components/Header';
+import styled, { css } from 'styled-components';
 import AddForm from '../components/AddForm';
 import Navbar from '../components/Navbar';
 
 export default function AddCharacter() {
   return (
-    <>
+    <Main>
       <Title>Create Character</Title>
       <AddForm />
       <Navbar />
-    </>
+    </Main>
   );
 }
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
