eb987dcc5e1f685141b81f359c388b1157cb7087
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-05-31 09:38:00 +0200

    Debug local storage

--- pages/characters.js
+++ pages/characters.js
@@ -1,15 +1,12 @@
 import { Title } from '../components/Header';
 import ShowCharacter from '../components/ShowCharacter';
 import Navbar from '../components/Navbar';
-import useStore from '../hooks/useStore';
 
 export default function Characters() {
-  const _hasHydrated = useStore(state => state._hasHydrated);
-  console.log(_hasHydrated);
   return (
     <>
       <Title>My Characters</Title>
-      {_hasHydrated && <ShowCharacter />}
+      <ShowCharacter />
       <Navbar />
     </>
   );
