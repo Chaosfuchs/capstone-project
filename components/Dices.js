7cadbc034e4af5b87c2fa91d7726f35d1a83ac52
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-06-02 13:51:36 +0200

    Add rough styling to the dice component

--- components/Dices.js
+++ components/Dices.js
@@ -11,25 +11,55 @@ export default function Dices() {
         <option>Dice 3</option>
       </select>
       <span>You rolled a ....</span>
-      <div>This will be the displayed Dice.</div>
+      <div>20</div>
       <button>Roll</button>
     </StyledDiceContainer>
   );
 }
 
 const StyledDiceContainer = styled.div`
-  background-color: lightgreen;
+  background-color: #1a9604;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
   padding: 10px;
   margin: 10px;
-  border-radius: 10px;
+  border-radius: 40px;
   border: 2px solid black;
   height: 240px;
 
+  select {
+    margin: 10px;
+    padding: 5px;
+    box-shadow: 5px 5px 10px black;
+  }
+
   div {
+    background-image: url(/dice-texture.png);
+    clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+    color: white;
+    font-size: 2rem;
+    text-align: center;
+    margin: 10px;
     padding: 30px;
+    width: 50px;
+    height: 50px;
+  }
+
+  button {
+    ${({ theme }) => css`
+      background-color: ${theme.colors.button};
+      box-shadow: ${theme.boxShadow.shadowHeavy};
+    `};
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 200px;
+    margin: 10px 30px;
+    padding: 10px;
     border: 2px solid black;
+    border-radius: 999px;
   }
 `;
