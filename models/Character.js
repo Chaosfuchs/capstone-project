59843995c17492c7ae77e0f0cbe686e6ad4c812e
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-06-16 09:01:05 +0200

    Add model for database

--- models/Character.js
+++ models/Character.js
@@ -3,9 +3,16 @@ import mongoose from 'mongoose';
 const CharacterSchema = new mongoose.Schema({
   name: {
     /* The name of this character */
-    type: String,
-    required: [true, 'Please provide a name for this user.'],
-    maxlength: [50, 'Name cannot be more than 50 characters'],
+    required: [true, 'Please provide a name for this character.'],
+    maxlength: [30, 'Name cannot be more than 50 characters'],
+  },
+  information: {
+    required: [true, 'Please provide a information for this character'],
+    maxlength: [3000, 'Information cannot be more than 3000 characters'],
+  },
+  type: {
+    required: [true, 'Please provide a name for this character.'],
+    maxlength: [30, 'Name cannot be more than 50 characters'],
   },
   image: {
     /* Characters image, if applicable */
