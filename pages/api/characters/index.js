b83ea571dc9304d1ab579683128bb6dd0239c70b
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-06-16 16:53:56 +0200

    Add delete function, connect whole app with mongo db

--- pages/api/characters/index.js
+++ pages/api/characters/index.js
@@ -4,7 +4,7 @@ import Character from '../../../models/Character';
 export default async function handler(request, response) {
   await dbConnect();
 
-  console.log(request.body);
+  console.log(-1, request.body);
 
   switch (request.method) {
     case 'POST':
@@ -18,24 +18,36 @@ export default async function handler(request, response) {
     case 'GET':
       try {
         const characters = await Character.find({});
-        response.status(204).json({ success: true, data: characters });
+        response.status(200).json({
+          success: true,
+          data: characters,
+        });
       } catch (error) {
         response.status(400).json({ success: false });
       }
       break;
     case 'PATCH':
       try {
-        const { _id, ...rest } = request.body;
-        const character = await Character.findByIdAndUpdate(_id, rest);
-        response.status(204).json({ success: true, data: character });
+        const { _id, ...update } = request.body;
+        let character = await Character.findByIdAndUpdate(_id, update);
+
+        if (character) {
+          character = await Character.findById(_id);
+          response.status(201).json({ success: true, data: character });
+        } else {
+          response.status(404).json({ success: false });
+        }
       } catch (error) {
         response.status(400).json({ success: false });
       }
       break;
     case 'DELETE':
+      console.log('Attempting to delete');
       try {
-        const character = await Character.findByIdAndDelete(request.body._id);
-        response.status(200).json({ success: true, data: character });
+        console.log(0, request.body);
+        const character = await Character.findByIdAndDelete(request.body.id);
+        console.log(1, character);
+        response.status(201).json({ success: true, data: character });
       } catch (error) {
         response.status(400).json({ success: false });
       }
